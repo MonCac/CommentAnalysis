c93ec7975b51ed71f7949ec6b1443f494ae3e703
gaoxujun <466626754@qq.com>
2023-05-22 17:36:42 +0800

    推荐排序

--- backend/apps/service/SelectSet/ChooseSort.py
+++ backend/apps/service/SelectSet/ChooseSort.py
@@ -1,7 +1,12 @@
 from pyspark.sql import HiveContext
-from SparkSessionBase import SparkSessionBase
+from backend.apps.service.SparkSessionBase import SparkSessionBase
 from pyspark.sql.functions import *
+from backend.apps.service.SelectSet import DefaultSort
 
+
+# 1. 添加排序功能，可根据点评推荐，距离，五星数量、评论数、平均分等条件进行排序，需要实现不同排序方式的功能。
+# 2. 依据不同的排序要求，由优到劣展示数据，并且支持添加多个排序条件，依据默认优先级进行排序展示。
+# 3. 如果未选择排序，则使用默认排序
 
 class TextRandJob(SparkSessionBase):
     SPARK_URL = "local"
@@ -11,35 +16,24 @@ class TextRandJob(SparkSessionBase):
     def __init__(self):
         self.spark = self._create_spark_session()
 
-    def moren(self):
+    # 一个筛选指标，
+    def choose(self, s):
         hc = HiveContext(self.spark.sparkContext)
         b_df = hc.table('business')
         r_df = hc.table('review')
 
         # 连接生成新表
-        new_df = b_df.join(r_df, b_df['business_id'] == r_df['rev_business_id']).distinct()
+        b_df = b_df.distinct()
+        new_df = b_df.join(r_df, r_df['rev_business_id'] == b_df['business_id'])
 
         # 默认排序
-        new_df.withColumn("score", new_df['stars'] * 100 + new_df['review_count']) \
-            .where(new_df['is_open'] == 1) \
-            .select(col('name'), col('score')) \
-            .orderBy(col('score').desc()).show()
-
-    def choose(self, s):
-        hc = HiveContext(self.spark.sparkContext)
-        b_df = hc.table('business')
-        # r_df = hc.table('review')
-
-        # 连接生成新表
-        new_df = b_df.distinct()
-
-        # 默认排序
-        new_df.withColumn("score", new_df['stars'] * 100 + new_df['review_count']) \
+        new_df.withColumn("五星数量", new_df['stars'] * 100 + new_df['review_count'])\
             .where(new_df['is_open'] == 1) \
             .select(col('name'), col(s)) \
-            .orderBy(col(s).desc()).show()
+            .orderBy(col(s).desc())\
+            .show(1500, truncate=False)
 
-    def choosecity(self,state,city):
+    def choosecity(self, state, city):
         hc = HiveContext(self.spark.sparkContext)
         b_df = hc.table('business')
         # r_df = hc.table('review')
@@ -48,10 +42,9 @@ class TextRandJob(SparkSessionBase):
         new_df = b_df.distinct()
 
         # 默认排序
-        new_df.withColumn("score", new_df['stars'] * 100 + new_df['review_count']) \
-            .where(new_df['is_open'] == 1) \
-            .where(col('state')==state)\
-            .where(col('city')==city)\
+        new_df.where(new_df['is_open'] == 1) \
+            .where(col('state') == state) \
+            .where(col('city') == city) \
             .select(col('name'), col(s)) \
             .orderBy(col(s).desc()).show()
 
@@ -59,13 +52,15 @@ class TextRandJob(SparkSessionBase):
 # XXX 大数据分析代码
 
 if __name__ == '__main__':
+
     # 点评推荐，距离，五星数量、评论数、平均分
+    num_choose = int(input("您有几个筛选指标："))
     num = int(input("请选择您的筛选指标："))
     newlist = ['default', 'stars', 'review_count', '五星好评数', 'city and state']
     print(newlist[num])
 
     if num == 0:
-        TextRandJob().moren()
+        DefaultSort.TextRandJob().start()
 
     if num == 4:
         state = input("请输入州名：")
@@ -73,8 +68,3 @@ if __name__ == '__main__':
 
     else:
         TextRandJob().choose(newlist[num])
-
-
-
-
-
--- backend/apps/service/SelectSet/DefaultSort.py
+++ backend/apps/service/SelectSet/DefaultSort.py
@@ -1,8 +1,7 @@
-from pyspark.ml.classification import LogisticRegression
+# 根据是否营业>评价（打卡）数量>评分结果>地理位置的标准对商家进行排序
+
 from pyspark.sql import HiveContext
-from SparkSessionBase import SparkSessionBase
-from pyspark.sql import SparkSession
-from pyspark.sql.types import *
+from backend.apps.service.SparkSessionBase import SparkSessionBase
 from pyspark.sql.functions import *
 
 
@@ -15,18 +14,18 @@ class TextRandJob(SparkSessionBase):
         self.spark = self._create_spark_session()
 
     def start(self):
+        # 读入数据
         hc = HiveContext(self.spark.sparkContext)
         b_df = hc.table('business')
 
-
-        # 连接
+        # 去重
         new_b_df = b_df.distinct()
 
-        # 加权
+        # 加权得分（权值设置后期可修改）
         new_b_df.withColumn("score", new_b_df['stars'] * 100 + new_b_df['review_count']) \
-            .where(new_b_df['is_open'] == 1)\
-            .select(col('name'), col('score'))\
-            .orderBy(col('score').desc()).show()
+            .where(new_b_df['is_open'] == 1) \
+            .select(col('name'), col('score')) \
+            .orderBy(col('score').desc()).show(1500, truncate=False)
 
 
 # XXX 大数据分析代码
