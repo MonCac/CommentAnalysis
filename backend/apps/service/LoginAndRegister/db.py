090f1733abb43c61c2da166e1ad950b220c11b83
abx <15465@qq.com>
2023-05-24 10:55:34 +0800

    登录与注册模块

--- backend/apps/service/LoginAndRegister/db.py
+++ backend/apps/service/LoginAndRegister/db.py
@@ -1,19 +1,17 @@
 import pymysql
 from pymysql import IntegrityError  # 引用内置函数
-from backend.apps.service.LoginAndRegister import main, storage
+from backend.apps.service.LoginAndRegister import main
 
 
 def query(username):  # 查询，校验注册及登录
     con = pymysql.connect(host='192.168.102.130', port=3306, user='root', password='abx2002', database='school',
                           charset='utf8')
-    # pymysql.connect固定写法，后面根据格式要求输入对应的连接信息，和php有些不一样的点在于，直接在连接里面设置utf8编码格式
-    cursor = con.cursor()  # 拿着游标在做查询
-    sql = f"select password from use where username='{username}';"
+    cursor = con.cursor()  # 游标做查询
+    sql = f"select password from pwd where username='{username}';"
     # 将sql语句参数化sql前面写个f里面的username给它用花括号参数化
-    cursor.execute(sql)  # 查sql语句的
+    cursor.execute(sql)  # 查sql语句
     results = (cursor.fetchone())  # 封装查询结果，fetchone一条，固定写法
     if results is None:  # 如果查询结果是空
-        storage.stor("system.log", f"‘{username}’用户不存在，登陆失败")  # 写入日志系统
         print(f"用户'{username}'不存在")  ##直接打印它不存在
         # 关闭查询，关闭数据数据库连接。保证占用资源损耗减少
         cursor.close()
@@ -26,36 +24,20 @@ def query(username):  # 查询，校验注册及登录
         return resul  # 给个返回值
 
 
-# 下面判断密码和电话的逻辑完全相同
-# 定义好连接，然后定好游标及sql修改语句并参数化
-# 执行连接和sql语句/固定写法cursor.execute()
-# ！！重重之中记得提交//固定写法con.commit()
-
-def dml_alter_pwd(password, username):  # 修改用户密码
+def dml_insert(username, password, status):  # 注册信息储存
     con = pymysql.connect(host='192.168.102.130', port=3306, user='root', password='abx2002', database='school',
                           charset='utf8')
     cursor = con.cursor()
-    sql = f"update use set password='{password}' where username='{username}';"
-    cursor.execute(sql)
-    con.commit()  # 提交
-
-
-def dml_insert(username, password):  # 注册信息储存
-    con = pymysql.connect(host='192.168.102.130', port=3306, user='root', password='abx2002', database='school',
-                          charset='utf8')
-    cursor = con.cursor()
-    sql = f"insert into use(username,password) values('{username}','{password}');"
+    sql = f"insert into pwd(username,password,status) values('{username}','{password}', '{status}');"
     # 这里注册信息用了个python报错异常处理try/except
     try:
         cursor.execute(sql)  # 执行
     except IntegrityError as e:  # 将这条开头的报错信息as成e
         if 'Duplicate' in str(e):  # 如果Dup在e报错里面
-            storage.stor("system.log", f"'{username}'用户已存在，请重新注册")  # 写入日志
             print('用户名重复，注册失败')  ##给用户提示
 
     else:  # 这里逻辑就简单了，如果里面没有用户传入的用户直接，else写数据库里
         con.commit()  # 直接提交
-        storage.stor("system.log", f"用户'{username}'注册成功")  # 写入日志
         print('注册成功')  # 提示成功
 
     finally:  # 不论啥结果，直接返回至菜单。
--- backend/apps/service/LoginAndRegister/login.py
+++ backend/apps/service/LoginAndRegister/login.py
@@ -1,60 +1,13 @@
-from backend.apps.service.LoginAndRegister import db, main, storage, check
+from backend.apps.service.LoginAndRegister import db, main
 
 
-def login1():  # 改密码
-    count = 0  ##定义个判断输错初始值
-    for i in range(3):  # 限定三次范围
-        username = input('请输入你的用户名：')
-        password = input('请输入你的密码：')
-        res = db.query(username)  ##请求一下数据库，看看有没有当前用户，锁定用户后面的用户信息也会跟着一起出来，结果res封装
-        if password != res:  ##这里不判断用户名，因为即便你随便输入的用户名，查找对应的密码信息，如果没有此用户，里面也没有随便输入的用户名的密码信息。
-            storage.stor("system.log", f"登录失败'{count + 1}'次")  ##将登录失败次数记录在log日志中
-            print('登录失败')  ##给登录失败提示
-            count += 1  ##失败一次直接给+1次
-            if count == 3:  ##如果输错3次
-                storage.stor("system.log", f"登录失败共'{count}'次")  ##统计次数记录日志中
-                print('你已登录失败3次，请登录成功后再修改密码')  ##给提示
-                main.select()  ##直接给他返回至主菜单页面
-                count = 0  ##次数清零，解决下次循环出现报错清空
-        else:
-            count = 0
-            storage.stor("system.log", f"用户'{username}'登录成功，")  ##否则直接登录成功，并记录日志，次数给归零
-            newpassword = input('请输入你的新密码：')  ##输入新密码
-            if check.check_pwd(newpassword) == True:  ##这里再次调用密码校验正则判断
-                newwpassword = input('请再次输入你的新密码：')  ##再次输入一次密码，为了让用户输入密码别再忘了，用于用户新密码加深记忆
-                if newpassword == newwpassword:  ##如果新输入的密码等于重新输入的新密码
-                    db.dml_alter_pwd(newwpassword, username)  ##直接将重新输入的新密码写入数据库，并且指定哪个用户的新密码
-                    storage.stor("system.log", f"用户‘{username}’修改密码成功")  ##写入日志某某修改成功
-                    print('密码修改成功')  ##给修改成功提示
-                    main.select()  ##返回主菜单页面
-                else:  ##否则
-                    storage.stor("system.log", f"2次输入的密码不一致，用户‘{username}’修改密码失败")  ##二次输入不一致，写入日志系统
-                    print('2次输入的密码不一致，修改密码失败')  ##给用户提示
-                    main.select()  ##直接回你老家吧，二次都失败了
-            else:  ##大if的判断，如果你输入的新密码不符合复杂度
-                storage.stor("system.log", "不符合密码复杂度要求，修改密码失败")  ##存入日志某某密码修改失败
-                print('不符合密码复杂度要求，修改密码失败')  ##给用户提示
-                main.select()  ##返回主菜单
-
-
-def loginfo():  # 包括登录成功和登录失败三次
-    count = 0  ##初始值给0
-    for i in range(3):  ##3次失败范围
+def login():  # 包括登录成功和登录失败三次
         username = input('请输入你的用户名：')
         password = input('请输入你的密码：')
         res = db.query(username)
         if password != res:
-            storage.stor("system.log", f"登录失败'{count + 1}'次")
-            print('登录失败')
-            count += 1
-            if count == 3:
-                storage.stor("system.log", f"登录失败共'{count}'次")
-                print('你已登录失败3次，返回主界面')
-                main.select()
-                count = 0
-        else:
-            storage.stor("system.log", f"用户‘{username}’登录成功")
-            print('登陆成功')
+            print('密码错误，登录失败')
             main.select()
-            count = 0
-# 整体思路如最上方一样，登录失败三次判断，记录日志，调用数据库用户信息数据，是否有。如果用户都写对，给他进入主菜单页面。
+        else:
+            print('登陆成功')
+            main.select()  # 用户主页
