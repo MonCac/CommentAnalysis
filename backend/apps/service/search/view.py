244245c8d00ef959ac456929c4cddcae208e90cc
abx <15465@qq.com>
2023-05-25 14:43:40 +0800

    搜索

--- backend/apps/service/search/view.py
+++ backend/apps/service/search/view.py
@@ -4,12 +4,12 @@ import pymysql
 # 正常搜索
 @search.route('/normalsearch')
 def normalSearch(params):
-    con = pymysql.connect(host='192.168.102.130', port=3306, user='root', password='abx2002', database='school',
+    con = pymysql.connect(host='192.168.102.130', port=3306, user='root', password='abx2002', database='yelp',
                           charset='utf8')
     cursor = con.cursor()  # 游标做查询
-    sql = f"select business_id, `name` from business where `name` like '%{params}%';"
+    sql = f"select business_id, `name` from business where `name` like '%{params}%' order by stars,review_count limit 10;"
     cursor.execute(sql)  # 查sql语句
-    results = (cursor.fetchone())  # 封装查询结果，fetchone一条，固定写法
+    results = (cursor.fetchall())  # 封装查询结果，fetchone一条，固定写法
     cursor.close()
     con.close()
     return results
@@ -18,12 +18,12 @@ def normalSearch(params):
 # 模糊搜索
 @search.route('/fuzzysearch')
 def fuzzySearch(params):
-    con = pymysql.connect(host='192.168.102.130', port=3306, user='root', password='abx2002', database='school',
+    con = pymysql.connect(host='192.168.102.130', port=3306, user='root', password='abx2002', database='yelp',
                           charset='utf8')
     cursor = con.cursor()  # 游标做查询
-    sql = f"select business_id, `name` from business where categories like '%{params}%';"
+    sql = f"select business_id, `name` from business where categories like '%{params}%' order by stars,review_count limit 10;"
     cursor.execute(sql)  # 查sql语句
-    results = (cursor.fetchone())  # 封装查询结果，fetchone一条，固定写法
+    results = (cursor.fetchall())  # 封装查询结果，fetchone一条，固定写法
     cursor.close()
     con.close()
     return results
