ea4bd231bcce516a47b58bc993e87d6a457d446e
abx <15465@qq.com>
2023-05-25 09:48:48 +0800

    搜索模块

--- backend/apps/service/search/view.py
+++ backend/apps/service/search/view.py
@@ -1,13 +1,29 @@
 from backend.apps.service.search import search
-
+import pymysql
 
 # 正常搜索
 @search.route('/normalsearch')
 def normalSearch(params):
-    return
+    con = pymysql.connect(host='192.168.102.130', port=3306, user='root', password='abx2002', database='school',
+                          charset='utf8')
+    cursor = con.cursor()  # 游标做查询
+    sql = f"select business_id, `name` from business where `name` like '%{params}%';"
+    cursor.execute(sql)  # 查sql语句
+    results = (cursor.fetchone())  # 封装查询结果，fetchone一条，固定写法
+    cursor.close()
+    con.close()
+    return results
 
 
 # 模糊搜索
 @search.route('/fuzzysearch')
 def fuzzySearch(params):
-    return
+    con = pymysql.connect(host='192.168.102.130', port=3306, user='root', password='abx2002', database='school',
+                          charset='utf8')
+    cursor = con.cursor()  # 游标做查询
+    sql = f"select business_id, `name` from business where categories like '%{params}%';"
+    cursor.execute(sql)  # 查sql语句
+    results = (cursor.fetchone())  # 封装查询结果，fetchone一条，固定写法
+    cursor.close()
+    con.close()
+    return results
